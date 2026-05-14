78ad496b63cf7c697bf69ab0dc0a8fa0d0afa851
Pa-del-cpu <pallavikhairnar1294@gmail.com>
2026-05-14 07:53:29 +0000

    conversion examples

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -6,9 +6,24 @@ let age = 14 //number
 let name = "Urvashi"  //string
 let isLoggedIn = true //boolean
 let countryName; //output = undefined //A variale that has been declared but not yet assigned any value. 
-let temprature  = Null //Null represents the intentional absence of any object value.
+let temprature  = null //Null represents the intentional absence of any object value.
 //BigInt used for larger integers. Specially used for facebook, tarding.
 // symbols = to indetufy uniqueness
 
-console.log(typeof name);
+const bigNumber = 126786543456789034567890n
+
+let MobileNO = 8654237790
+
+console.log(typeof temprature);
+console.log(typeof isLoggedIn);
+console.log(typeof countryName);
+console.log(typeof "Urvashi");
+console.log(typeof bigNumber);
+console.log(typeof MobileNO);
 console.log(typeof age);
+console.log(typeof 8654237790)
+console.log(typeof 126786543456789034567890n)
+console.log(typeof true)
+
+
+
--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,4 +1,43 @@
-let score = 33
+let score = "33"
 
-console.log(typeof score)
-console.log(typeof(score))
+console.log(typeof score) //to identify the type of variable
+console.log(typeof(score)) ///to identify the type of variable
+
+let valueInNumber = Number(score) //converted into numeric value
+console.log(typeof valueInNumber) //confirm the datatype of valueInNumber variable
+
+let score1 = null // with null value
+console.log(typeof score1);
+
+let valueInNumber1 = Number(score1)
+console.log (typeof valueInNumber1)
+
+let score2 // with undefined value
+console.log(typeof score2)
+
+let valueInNumber2 = Number(score2)
+console.log (typeof valueInNumber2);
+
+console.log (valueInNumber)
+
+console.log(valueInNumber2) //print the value stored inside the variable
+
+//33// => 33
+//33abc// = NAN
+// true => 1, false => 0
+//undefined = Nan
+
+
+//expression = outpu
+//null = null
+//number (null) = 0
+//typeof (null) = "object"
+//Boolean (null) = false
+
+let isLoggedIn = 1
+
+let BooleannIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof BooleannIsLoggedIn)
+
+// "  " => false
+// pallavi => true
