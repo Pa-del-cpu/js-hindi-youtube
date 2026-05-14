97fb7395c965c9395a0e3e2b8db74280b8a1ce27
Pa-del-cpu <pallavikhairnar1294@gmail.com>
2026-05-14 09:22:11 +0000

    conversion  applied

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -41,3 +41,11 @@ console.log(typeof BooleannIsLoggedIn)
 
 // "  " => false
 // pallavi => true
+
+*****************************************************************
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
