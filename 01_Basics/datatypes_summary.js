ed5caa256e21853e8bb7150519926863a238e674
Pa-del-cpu <pallavikhairnar1294@gmail.com>
2026-05-14 13:16:27 +0000

    datatypes with examples

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,31 @@
+/// datatypes
+
+//1. primitive
+// string, number, boolean, null, undefined, symbol, BigInt
+
+const score = 100  // number type
+const scoreValue = 13.5 // number type
+
+const isLoggedIn = false //Boolean type
+const outsideTemp = null // null type not zero but empty
+
+let euesrEmail; // undefined data type
+
+const id = Symbol('123') //symbol data type
+const anotherId = Symbol('123') //symbol data type
+
+console.log(id===another);
+
+const bigNumber = 84734353888456789678n // \bigInt data type
+
+//2. Reference or non primitive
+//Array, objects, functions
+
+const heros = ["shaktiamn", "naagraj", "doga"] // Array data type
+
+let myobj = {
+    name: "hitesh",
+    age: 25,
+}                      // Objects data type
+
+const myfunction 
