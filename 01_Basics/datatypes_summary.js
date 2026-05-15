60ac381a9635c2cd46826fec0f0aae15bf0506e6
Pa-del-cpu <pallavikhairnar1294@gmail.com>
2026-05-15 06:27:05 +0000

    datatypes with memory

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -14,7 +14,7 @@ let euesrEmail; // undefined data type
 const id = Symbol('123') //symbol data type
 const anotherId = Symbol('123') //symbol data type
 
-console.log(id===another);
+console.log(id===anotherId);
 
 const bigNumber = 84734353888456789678n // \bigInt data type
 
@@ -28,4 +28,37 @@ let myobj = {
     age: 25,
 }                      // Objects data type
 
-const myfunction 
+
+///**************************************Memory*********************/
+
+//stack = primitive Data Type and Heap = non primitive data type
+
+// Stack
+
+let myYoutubeName = "pallavikhairnardotcom"
+
+let anotherName = myYoutubeName
+anotherName = "chaiAurCode"
+
+console.log(anotherName);
+console.log(myYoutubeName);
+
+// create copy of the variable. Original value remain as it is.
+// Changes made in the copy so orginal value remain as it is.
+
+// heap
+
+let user = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+ let userTwo = user
+
+ userTwo.email = "pallavi@google.com"
+
+ console.log(user.email);
+ console.log(userTwo.email);
+
+ // in this, variables are taking reference of the same set of value.
+ // whatever value we are updating we are updating it in going to update in the original value.
+  
