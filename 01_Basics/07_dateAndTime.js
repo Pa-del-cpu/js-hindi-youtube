57895afc50f6885f9014daec654099704de95b77
Pa-del-cpu <pallavikhairnar1294@gmail.com>
2026-05-18 05:51:22 +0000

    dateandTime data

---
+++ 01_Basics/07_dateAndTime.js
@@ -0,0 +1,20 @@
+let myDate = new Date()
+
+console.log(myDate.toString())
+console.log(myDate.getDate())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toTimeString())
+
+//Date is a object in JavaScript.
+
+let myCreateDate = new Date(2026, 4, 23)
+console.log(myCreateDate.toDateString())
+
+
+
+
+
