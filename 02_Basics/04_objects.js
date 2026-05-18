9732de287701100fb72582809f0f4fea0c6ab7c7
Pa-del-cpu <pallavikhairnar1294@gmail.com>
2026-05-18 12:43:16 +0000

    recent changes

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -44,4 +44,4 @@ console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty(`isLoggedIn`)); //is used to check wheather object use specific property or not
+console.log(tinderUser.hasOwnProperty(`isLoggedIn`)); //is used to check wheather object use specific property or not.
